cebf6141965e81edcd8d67b5d8e047a588f75c15
trikitrok <trikitrok@gmail.com>
2015-09-23 00:16:49 +0200

    using text instead of word in facts descriptions

--- test/word_wrap/core_test.clj
+++ test/word_wrap/core_test.clj
@@ -6,21 +6,21 @@
   "about wrapping words"
 
   (fact
-    "words that fit in the given columns number are not wrapped"
+    "a text that fits in the given columns number are not wrapped"
     (wrap "koko koko" 9) => "koko koko"
     (wrap "ko" 9) => "ko"
     (wrap "" 9) => "")
 
   (fact
-    "words without spaces that don't fit in the given columns number are wrapped"
+    "a text without spaces that doesn't fit in the given columns number are wrapped"
     (wrap "kokokoko" 4) => "koko\nkoko")
 
   (fact
-    "words with spaces that don't fit in the given columns number are wrapped
+    "a text with spaces that doesn't fit in the given columns number are wrapped
     at the space that is closest to the maximum column"
     (wrap "koko koko" 7) => "koko\nkoko"
     (wrap "koko koko koko" 12) => "koko koko\nkoko"
     (wrap "koko koko koko koko koko koko" 12) => "koko koko\nkoko koko\nkoko koko"
     (wrap
-      "This kata should be easy unless there are hidden, or not so hidden, obstacles. Let's start!"
-      12) => "This kata\nshould be\neasy unless\nthere are\nhidden, or\nnot so\nhidden,\nobstacles.\nLet's start!"))
+      "This koko should be easy unless there are hidden, or not so hidden, obstacles. Let's start!"
+      12) => "This koko\nshould be\neasy unless\nthere are\nhidden, or\nnot so\nhidden,\nobstacles.\nLet's start!"))
