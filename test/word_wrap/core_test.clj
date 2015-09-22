34256d8de94c23998b173d183465d6f8c6e89a19
trikitrok <trikitrok@gmail.com>
2015-09-23 00:14:35 +0200

    Added some more tests to gain confidence in solution

--- test/word_wrap/core_test.clj
+++ test/word_wrap/core_test.clj
@@ -7,7 +7,9 @@
 
   (fact
     "words that fit in the given columns number are not wrapped"
-    (wrap "koko koko" 9) => "koko koko")
+    (wrap "koko koko" 9) => "koko koko"
+    (wrap "ko" 9) => "ko"
+    (wrap "" 9) => "")
 
   (fact
     "words without spaces that don't fit in the given columns number are wrapped"
@@ -18,4 +20,7 @@
     at the space that is closest to the maximum column"
     (wrap "koko koko" 7) => "koko\nkoko"
     (wrap "koko koko koko" 12) => "koko koko\nkoko"
-    (wrap "koko koko koko koko koko koko" 12) => "koko koko\nkoko koko\nkoko koko"))
+    (wrap "koko koko koko koko koko koko" 12) => "koko koko\nkoko koko\nkoko koko"
+    (wrap
+      "This kata should be easy unless there are hidden, or not so hidden, obstacles. Let's start!"
+      12) => "This kata\nshould be\neasy unless\nthere are\nhidden, or\nnot so\nhidden,\nobstacles.\nLet's start!"))
