979afafda7036474734031cac0f9ac1e3b2fe6fe
trikitrok <trikitrok@gmail.com>
2015-09-23 01:40:01 +0200

    Renamed parameter

--- src/word_wrap/core.clj
+++ src/word_wrap/core.clj
@@ -15,45 +15,45 @@
 (defn- spaces-indexes [line]
   (indexes (filter space? (map-indexed #(vector %1 %2) line))))
 
-(defn- fitting-spaces-indexes [line num-columns]
-  (filter #(< % num-columns) (spaces-indexes line)))
+(defn- fitting-spaces-indexes [line max-columns]
+  (filter #(< % max-columns) (spaces-indexes line)))
 
 (def ^:private not-found -1)
 
-(defn- index-of-last-fitting-space [line num-columns]
-  (if-let [index (last (fitting-spaces-indexes line num-columns))]
+(defn- index-of-last-fitting-space [line max-columns]
+  (if-let [index (last (fitting-spaces-indexes line max-columns))]
     index
     not-found))
 
 (def ^:private valid-index? pos?)
 
-(defn- compute-wrapping-index [line num-columns]
-  (let [index (index-of-last-fitting-space line num-columns)]
+(defn- compute-wrapping-index [line max-columns]
+  (let [index (index-of-last-fitting-space line max-columns)]
     (if (valid-index? index)
       index
-      num-columns)))
+      max-columns)))
 
-(defn- fits? [line num-columns]
-  (<= (count line) num-columns))
+(defn- fits? [line max-columns]
+  (<= (count line) max-columns))
 
-(defn- line->wrapped-lines [wrapped-lines line num-columns]
-  (if (fits? line num-columns)
+(defn- line->wrapped-lines [wrapped-lines line max-columns]
+  (if (fits? line max-columns)
     (conj wrapped-lines line)
-    (let [index (compute-wrapping-index line num-columns)]
+    (let [index (compute-wrapping-index line max-columns)]
       (recur (conj wrapped-lines (wrap-line-at line index))
              (rest-of-line line index)
-             num-columns))))
+             max-columns))))
 
-(defn- wrap-line [line num-columns]
-  (apply str (line->wrapped-lines [] line num-columns)))
+(defn- wrap-line [line max-columns]
+  (apply str (line->wrapped-lines [] line max-columns)))
 
 (defn- extract-lines [text]
   (string/split text #"\n"))
 
 (def ^:private join-lines (partial string/join "\n"))
 
-(defn wrap [text num-columns]
+(defn wrap [text max-columns]
   (->> text
        extract-lines
-       (map #(wrap-line % num-columns))
+       (map #(wrap-line % max-columns))
        join-lines))
